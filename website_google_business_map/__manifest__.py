083d9f81bb2b96cec484145e07813d639c76110b
Off-IT <geoffroy@off-it.be>
2020-06-11 13:30:12 +0200

    Initial commit

---
+++ website_google_business_map/__manifest__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Google Business Map',
+    'category': 'Website',
+    'summary': 'Show your business location on a Google Map',
+    'version': '1.0',
+    'description': """
+Show your business location on a Google Map. Add a snippet on your website to show an embeded map. Put the embed url link obtained at google in the Website Google Plus setting.
+    """,
+    'author': 'Off-IT',
+    'depends': ['website'],
+    'data': [
+        'views/google_business_map_templates.xml',
+    ],
+    'installable': True,
+}
